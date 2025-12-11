f3c60e4c5399a1b29a1cd279760f19eb57790644
Aritz Ontalvilla <aritzontal@gmail.com>
2025-12-11 22:39:12 +0100

    arreglado tema para android por wifi

--- app/src/main/java/com/svalero/musicrightsapp/api/ConcertApi.java
+++ app/src/main/java/com/svalero/musicrightsapp/api/ConcertApi.java
@@ -8,7 +8,8 @@ public class ConcertApi {
     // Construye la conexión
     public static ConcertApiInterface buildInstance() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://10.0.2.2:8080/") // IP especial para el emulador
+                //.baseUrl("http://10.0.2.2:8080/") //IP PARA RED WI-FI
+                .baseUrl("http://192.168.1.129:8080/")// IP especial para el emulador
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
