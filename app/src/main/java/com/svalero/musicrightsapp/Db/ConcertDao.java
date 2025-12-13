72e36ea85e1a84a7ab9e576d41393f2f92cfcf3e
Aritz Ontalvilla <aritzontal@gmail.com>
2025-12-13 10:58:26 +0100

    arreglo centrar posicion point en mapa

--- app/src/main/java/com/svalero/musicrightsapp/Db/ConcertDao.java
+++ app/src/main/java/com/svalero/musicrightsapp/Db/ConcertDao.java
@@ -1,7 +1,6 @@
     package com.svalero.musicrightsapp.database;
 
     import androidx.room.Dao;
-    import androidx.room.Delete;
     import androidx.room.Insert;
     import androidx.room.OnConflictStrategy;
     import androidx.room.Query;
@@ -25,7 +24,4 @@
 
         @Update
         void update(Concert concert);
-
-        @Query("DELETE FROM concert WHERE showTitle = :id")
-        void deleteAll(long id);
     }
