7304aad6f2d0efc3fbd4111cfa59ad30dce7dae1
Aritz Ontalvilla <aritzontal@gmail.com>
2026-01-18 11:40:02 +0100

    retrofit cambiado para emulador Android

--- app/src/main/java/com/svalero/musicrightsapp/api/ConcertApi.java
+++ app/src/main/java/com/svalero/musicrightsapp/api/ConcertApi.java
@@ -8,8 +8,8 @@ public class ConcertApi {
     // Construye la conexión
     public static ConcertApiInterface buildInstance() {
         Retrofit retrofit = new Retrofit.Builder()
-                //.baseUrl("http://10.0.2.2:8080/") //IP PARA RED WI-FI
-                .baseUrl("http://192.168.1.129:8080/")// IP especial para el emulador
+                .baseUrl("http://10.0.2.2:8080/") //IP PARA RED EMULADOR
+                //.baseUrl("http://192.168.1.129:8080/")// IP DE MI WIFI
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
